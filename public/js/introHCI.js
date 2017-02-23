e3638c68cd577ac837c7aadd102a51ce0a0b9380
Noah Lee <nol006@ucsd.edu>
2017-02-23 10:02:05 -0800

    Part 1 of Lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(function(e){
+		e.preventDefault();
+		ga("send", "event", "like", "click");
+	});
 }
